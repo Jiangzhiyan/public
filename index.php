a8c1a14d6113a7dc2505461fdb759700cf1146ee
Jiangzhiyan <47499807+Jiangzhiyan@users.noreply.github.com>
2019-04-05 12:56:45 +0800

    number to en

--- index.php
+++ index.php
@@ -5,32 +5,36 @@
     <title> Test Page </title>
   </head>
   <body>
-    <h1> Test Page For Rolling </h1>
+    <h1> Test Page of For </h1>
     <p>
       <?php
 
         $roll1 = 0;
         $roll2 = 0;
 
-        for ($i = 0; $i < 10; $i++) {
+        $en = [
+          1 => 'one',
+          2 => 'two',
+          3 => 'three',
+          4 => 'four',
+          5 => 'five',
+          6 => 'six'
+        ];
 
-          $roll1 = rand(1, 6);
-          $roll2 = rand(1, 6);
+        $roll1 = rand(1, 6);
+        $roll2 = rand(1, 6);
 
-          echo '<p>You rolled a ' . $roll1 . ' and a ' . $roll2 . '.</p>';
+        echo '<p>You rolled a ' . $en[$roll1] . ' and a ' . $en[$roll2] . '.</p>';
 
-          if ($roll1 == 6 && $roll2 == 6) {
+        if ($roll1 == 6 && $roll2 == 6) {
 
-            echo '<p> You win! </p>';
+          echo '<p> You win! </p>';
 
-          } else {
+        } else {
 
-            echo '<p> Sorry, you didn\'t win, better luck next time! </p>';
+          echo '<p> Sorry, you didn\'t win, better luck next time! </p>';
 
-          }
         }
-
-
 
         echo '<p> Thank you for playing </p>';
 

ee0834239a16cef5ec4abfd84ed4287b4e0a6b3a
Jiangzhiyan <47499807+Jiangzhiyan@users.noreply.github.com>
2019-04-05 17:29:12 +0800

    include

--- index.php
+++ index.php
@@ -6,14 +6,10 @@
   </head>
   <body>
     <h1> Test Pages of Get </h1>
+
     <?php
 
-      $firstname = $_GET['firstname'];
-      $lastname = $_GET['lastname'];
-      
-      echo 'Welcome to our website, ' .
-        htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' .
-        htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
+      echo $output;
 
     ?>
 
